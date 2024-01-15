44ff677260495d0109aaf449ed9fcebcf4f47a76
Stine Kvist <stinekvist@Stines-MacBook-Air-2.local>
2024-01-15 21:36:13 +0100

    updatet the color names

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import chalk from "chalk";
 const log = console.log;
 
-log(chalk.blue('What') + chalk.red('did') + chalk.yellow('the') + chalk.green('ocean') + chalk.black('say') + chalk.white('to') + chalk.orange('the')
-+ chalk.purple('beach?')+ chalk.blue('Nothing')+ chalk.red('it')+ chalk.yellow('just')+ chalk.green('waved.'));
+log(chalk.blue('What') + chalk.red('did') + chalk.yellow('the') + chalk.green('ocean') + chalk.black('say') + chalk.white('to') + chalk.cyan('the')
++ chalk.magenta('beach?')+ chalk.blue('Nothing')+ chalk.red('it')+ chalk.yellow('just')+ chalk.green('waved.'));
  
 
 
