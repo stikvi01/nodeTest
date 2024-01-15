65a3befb2b19d4d4f2336256a3359eb1f425032f
Stine Kvist <stinekvist@Stines-MacBook-Air-2.local>
2024-01-15 21:30:57 +0100

    added the joke

---
+++ app.js
@@ -0,0 +1,10 @@
+import chalk from "chalk";
+const log = console.log;
+
+log(chalk.blue('What') + chalk.red('did') + chalk.yellow('the') + chalk.green('ocean') + chalk.black('say') + chalk.white('to') + chalk.orange('the')
++ chalk.purple('beach?')+ chalk.blue('Nothing')+ chalk.red('it')+ chalk.yellow('just')+ chalk.green('waved.'));
+ 
+
+
+
+
